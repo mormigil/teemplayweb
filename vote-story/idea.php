c94fd19d10c0cf76b3c5afc76b3d11336b391abd
Zachary <zbarryte@mit.edu>
2013-09-03 18:21:58 -0400

    copied vote-page to make a vote-story page

---
+++ vote-story/idea.php
@@ -0,0 +1,68 @@
+<?php
+class idea{
+	private $id;
+	private $votes;
+
+	private function __construct($id, $votes){
+		$this->id = $id;
+		$this->votes = $votes;
+	}
+
+	public static function findAll(){
+		$mysqli = new mysqli("localhost", "root", "", "nu");
+		$result = $mysqli->query("SELECT * FROM tbl_ideas");
+		$ideas = array();
+		if($result){
+		if($result->num_rows == 0){
+			return null;
+		}
+		for($i=1;$i<=9;$i++){
+			$idea_info = $result->fetch_array();
+			if($idea_info){
+				$ideas[] = idea::findByID($idea_info[0]);
+			}
+		}
+		}
+		return $ideas;
+	}
+
+	public static function findByID($id){
+		$mysqli = new mysqli("localhost", "root", "", "nu");
+		$result = $mysqli->query("SELECT * FROM tbl_ideas WHERE id = ".$id);
+		if($result){
+		if($result->num_rows == 0){
+			return null;
+		}
+		$idea_info = $result->fetch_array();
+		return new idea(intval($idea_info['id']), intval($idea_info['votes']));
+		}
+		return null;
+	}
+
+	public function vote(){
+		$this->votes = $this->votes + 1;
+		return $this->update();
+	}
+
+	public function removeVote(){
+		$this->votes = $this->votes - 1;
+		return $this->update();
+	}
+
+	public function update(){
+		$mysqli = new mysqli("localhost", "root", "", "nu");
+		$user_id = $_SERVER['REMOTE_ADDR'];
+                $time = time();
+		$result2 = $mysqli->query("INSERT INTO tbl_records (id, user, vote, timestamp) VALUES (NULL, '$user_id', '$this->id', '$time')");
+		$result = $mysqli->query("UPDATE tbl_ideas SET votes = ". $this->votes ." WHERE id = ". $this->id);
+		return $result;
+	}
+
+	public function getJSON(){
+		$json_rep = array();
+		$json_rep['id'] = $this->id;
+		$json_rep['votes'] = $this->votes;
+		return $json_rep;
+	}
+}
+?>
---
+++ vote-story/vote.js
@@ -0,0 +1,71 @@
+function voted(id){
+	$('.vote').remove();
+	var total = 0;
+	var percent = 0;
+	var votes = new Array();
+	$.ajax({
+		type: 'GET',
+		url: 'ideas.php',
+		dataType: 'json',
+		success: function(data){
+			for(var i=0; i<9; i++){
+				total = total + data[i].votes;
+				votes[i] = data[i].votes;
+			}
+			for(var i = 0; i<9; i++){
+				percent = 100*data[i].votes;
+				percent = percent/total;
+				percent = Math.round(percent*10);
+				percent = percent/10;
+				$("#changeable"+(i+1)).append("<p class = 'voted'>"+percent + "%</p>");
+			}
+			$('#box'+id).addClass("selected");
+			$('.box').addClass('notselected');
+			$('#box'+id).removeClass('notselected');
+		},
+		error: function(data, error){
+        console.debug(data); 
+        console.debug(error);
+    	}
+    	
+	});
+/*
+	$.get("ideas.php", function(data) {
+		total = data[0].votes;
+		alert("data loaded" + total);
+	}, "json");*/
+}
+
+$(document).ready(function(){
+	var c_value = document.cookie;
+
+	var c_start = c_value.indexOf(" onevote=");
+	if (c_start == -1)
+  	{
+  		c_start = c_value.indexOf("onevote=");
+  	}
+	if (c_start == -1)
+  	{
+  		c_value = null;
+  	}
+	else
+  	{
+  		c_start = c_value.indexOf("=", c_start) + 1;
+  		var c_end = c_value.indexOf(";", c_start);
+  	if (c_end == -1)
+  	{
+		c_end = c_value.length;
+	}
+	c_value = unescape(c_value.substring(c_start,c_end));
+	}
+	if(c_value!=null){
+		c_value = parseInt(c_value);
+		voted(c_value);
+	}
+});
+$(document).on('click', '.vote', function(){
+	$.post("ideas.php", {id: $(this).val()}, function(data){
+	}, "json");
+	alert("Thank you for your vote please come back tomorrow to cast another!");
+	voted($(this).val());
+});
