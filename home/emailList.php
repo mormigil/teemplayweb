7e1dec80b135b90d1bf8a03916a116c3759d252b
mormigil <ankh579@yahoo.com>
2013-10-11 13:34:07 -0400

    home stuff but this shouldn't be changing

--- home/emailList.php
+++ home/emailList.php
@@ -6,11 +6,19 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 		exit();
 	}
 	else{
-		$mysqli = new mysqli("localhost:3306", "root", "", "teemplayweb");
-		$query = "INSERT INTO emailList (id, email) VALUES (?, ?)";
+		$mysqli = new mysqli("localhost", "teemplay_morm", "3EeG9nTn6lid", "teemplay_web");
+		$query = "INSERT INTO emaillist (email) VALUES (?)";
 		$prep = $mysqli->prepare($query);
 		$prep->bind_param('s', $_POST['email']);
-		return $_POST['email'];
+		$prep->execute();
+		if($mysqli->error){
+			printf("Errormessage: %s\n", $mysqli->error);
+			return false;
+		}
+		$json_data = array();
+		header("Content-type: application/json");
+		print(json_encode($json_data));
+		exit();
 	}
 }
 ?>
--- home/home.js
+++ home/home.js
@@ -1,17 +1,38 @@
 /*home.js*/
 
-$(document).on("click", "#signup", function(){
-	var email = $("#email").val();
-	if(validateEmail(email)){
-		$.post("emailList.php", {email: email}, function(){
-			alert("Thank you for signing up. We'll be in contact soon!");
-		});
-	}
+$(document).keyup(function (e) {
+    if (("#email:focus")&&(e.keyCode == 13)) {
+        submit();
+    }
 });
 
-function validateEmail(email) { 
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\
-".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA
--Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+$(document).on("click", "#signup", function(){
+	submit();
+});
+
+function submit(){
+	var email = $("#email").val();
+	if(validateEmail(email)){
+		$.ajax({
+			type: "POST",
+			url: "emaillist.php",
+			data: {email: email},
+			dataType: 'json',
+			success: function(data){
+				alert("Thank you for signing up. We'll be in contact soon!");
+			},
+			error: function(jqXHR, textStatus, errorThrown){
+				alert(errorThrown);
+				alert("Error submitting your email. You've probably already signed up.");
+			}
+		});
+	}
+	else{
+		alert("Invalid email address. Please submit a full email address typically of format s@dom.ain");
+	}
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
-} 
+}
